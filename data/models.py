d6eb8bf294d9bd53fce0e271bc440ac1d4db1bf7
Kevin Soto Avila <kssoto29@ucatolica.edu.co>
2025-04-29 08:13:12 -0500

    Update models.py

--- data/models.py
+++ data/models.py
@@ -1,4 +1,15 @@
-'''Aqui debes consignar el modelo que se te indico en el parcial
-Escribe aquí el que te corresponde.
+from sqlmodel import Field,SQLModel
+from typing import Optional, List
+from enum import Enum
 
-'''
+class Usuario(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str=Field(nullable=False)
+    email:str=Field(nullable=False)
+    estado:Estadosuario=Field(default=EstadoUsuario.ACTIVO)
+    premium:bool=Field(nullable=False)
+
+class EstadoUsuario(str,Enum):
+    ACTIVO = "ACTIVO"
+    INACTIVO = "INACTIVO"
+    ELIminado = "ELIMINADO"
