cf684b260f3ce45e62b3d68242fbac61d1a6cb5e
Kevin <kssoto29@ucatolica.edu.co>
2025-05-07 17:15:28 -0500

    agregando clase tarea y estadotarea

--- data/models.py
+++ data/models.py
@@ -1,11 +1,21 @@
 from sqlmodel import Field, SQLModel
 from typing import Optional
 from enum import Enum
+from datetime import datetime
+
 
 class EstadoUsuario(str, Enum):
     ACTIVO = "ACTIVO"
     INACTIVO = "INACTIVO"
     ELIMINADO = "ELIMINADO"
+
+
+class EstadoTarea(str, Enum):
+    PENDIENTE = "PENDIENTE"
+    EN_PROGRESO = "EN_PROGRESO"
+    COMPLETADA = "COMPLETADA"
+    CANCELADA = "CANCELADA"
+
 
 class Usuario(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -13,3 +23,13 @@ class Usuario(SQLModel, table=True):
     email: str
     estado: EstadoUsuario = Field(default=EstadoUsuario.ACTIVO)
     premium: bool
+
+
+class Tarea(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str = Field(nullable=False)
+    descripcion: str = Field(nullable=False)
+    fecha_creacion: datetime = Field(default_factory=datetime.now)
+    fecha_modificacion: Optional[datetime] = Field(default=None, nullable=True)
+    estado: EstadoTarea = Field(default=EstadoTarea.PENDIENTE)
+    usuario_id: Optional[int] = Field(default=None, foreign_key="usuario.id")
